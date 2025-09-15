e6f397fafae5f3f93edf3fc9e504dfb63653c048
Martin.Haluska <martin.haluska@lseg.com>
2025-09-15 19:05:50 +0200

    Fix app.py

--- 1.Docker Container/app.py
+++ 1.Docker Container/app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
-
 app = Flask(__name__)
+pod_name = os.environ.get('HOSTNAME', 'unknown')
 
 @app.route("/")
 def hello():
-    return "Hello world"
+    return f"Hello from pod {pod_name}!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
