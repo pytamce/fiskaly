84b4bf6711973d0c924b673a232d66ef4dfe2c78
Martin.Haluska <martin.haluska@lseg.com>
2025-09-15 16:52:19 +0200

    Add terraform module for IaC

---
+++ 3. Infrastructure as Code/cluster.tf
@@ -0,0 +1,115 @@
+resource "aws_iam_role" "demo-eks-cluster-role" {
+name = "demo-eks-cluster-role"
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+    {
+        Action = [
+        "sts:AssumeRole",
+        ]
+        Effect = "Allow"
+        Principal = {
+        Service = "eks.amazonaws.com"
+
+        }
+    },
+    ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+role       = aws_iam_role.demo-eks-cluster-role.name
+}
+
+# my IP
+data "http" "my_ip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+resource "aws_eks_cluster" "demo-eks-cluster" {
+    name = var.cluster_name
+    role_arn = aws_iam_role.demo-eks-cluster-role.arn
+    vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access = true
+    subnet_ids =concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
+    public_access_cidrs    = ["${chomp(data.http.my_ip.response_body)}/32"]
+    }
+    access_config {
+    authentication_mode = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+    }
+    bootstrap_self_managed_addons = true
+    tags = var.tags
+    version = var.eks_version
+    upgrade_policy {
+    support_type = "STANDARD"
+    }
+    depends_on = [ aws_iam_role_policy_attachment.eks_cluster_policy ]
+}
+
+
+
+
+
+
+
+resource "aws_iam_role" "demo-eks-ng-role" {
+name = "demo-eks-node-group-role"
+
+assume_role_policy = jsonencode({
+    Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+        Service = "ec2.amazonaws.com"
+    }
+    }]
+    Version = "2012-10-17"
+})
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-ng-WorkerNodePolicy" {
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+role       = aws_iam_role.demo-eks-ng-role.name 
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-ng-AmazonEKS_CNI_Policy" {
+policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+role       = aws_iam_role.demo-eks-ng-role.name 
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-ng-ContainerRegistryReadOnly" {
+policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+role       = aws_iam_role.demo-eks-ng-role.name 
+}
+
+resource "aws_eks_node_group" "eks-demo-node-group" {
+cluster_name    = var.cluster_name
+node_role_arn   = aws_iam_role.demo-eks-ng-role.arn
+node_group_name = "demo-eks-node-group"
+subnet_ids      = [
+    aws_subnet.private-subnet-1.id, 
+    aws_subnet.private-subnet-2.id
+    ]
+instance_types = "t3.medium"
+ami_type      = "AL2023_ARM_64_STANDARD"
+
+scaling_config {
+    desired_size = 4
+    max_size     = 4
+    min_size     = 4
+}
+update_config {
+    max_unavailable = 1
+}
+
+# Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+# Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+depends_on = [
+    aws_iam_role_policy_attachment.eks-demo-ng-WorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-demo-ng-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-demo-ng-ContainerRegistryReadOnly,
+]
+}
---
+++ 3. Infrastructure as Code/security.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "eks_nodes" {
+  name        = "eks-nodes-sg"
+  description = "Allow communication for EKS worker nodes"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "lb" {
+  name   = "hello-lb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
