e78bd12942f15f4e5b549c4acbafe449f0b6efcf
Martin.Haluska <martin.haluska@lseg.com>
2025-09-25 22:41:36 +0200

    fix scalling

--- 3.Infrastructure as Code/modules/cluster/main.tf
+++ 3.Infrastructure as Code/modules/cluster/main.tf
@@ -108,9 +108,9 @@ resource "aws_eks_node_group" "eks-node-group" {
   ami_type       = var.ami_type
 
   scaling_config {
-    desired_size = 4
+    desired_size = 2
     max_size     = 4
-    min_size     = 4
+    min_size     = 2
   }
   update_config {
     max_unavailable = 1
